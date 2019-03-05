43173f02a7ca26eee6bcec6ae4d626be910af01e
Kunwar gaurav singh <mail.kunwargaurav@gmail.com>
2019-03-06 00:58:26 +0530

    excercise files

---
+++ 2-modern-js/2-const.js
@@ -0,0 +1,13 @@
+// Scalar values
+const answer = 42;
+const greeting = 'Hello';
+
+// Arrays and Objects
+const numbers = [2, 4, 6];
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+// const is assiged to unique reference and for scalars like int or strig it can't be mutated.
+// But in case of object or arrays, reference won't be changed but array or object's value can be altered.
